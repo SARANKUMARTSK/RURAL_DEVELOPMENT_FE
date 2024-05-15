ff9afa52bb4efa9c28ec0af8da47e48fba5366b9
SARANKUMARTSK <sarankumartsk@gmail.com>
2024-05-15 11:13:42 +0530

    Complaint Page Added

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import LandingPage from './pages/LandingPage/LandingPage'
 import SignUp from './pages/AuthPage/SignUp'
 import ForgotPassword from './pages/AuthPage/ForgotPassword'
 import ResetPassword from './pages/AuthPage/ResetPassword'
-import Complaint from './pages/AppPage/Complaint'
+import Complaint from './pages/AppPage/complaint/Complaint'
 import DefaultPage from './components/DefaultPage'
 import Dashboard from './pages/AppPage/Dashboard'
+import TrackComplaint from './pages/AppPage/complaint/TrackComplaint'
+import EditComplaint from './pages/AppPage/complaint/EditComplaint'
 
-// export  const API_URL ="http://localhost:8001"
-export const API_URL = "https://rural-development-be.onrender.com"
+export  const API_URL ="http://localhost:8001"
+// export const API_URL = "https://rural-development-be.onrender.com"
 
 
 function App() {
@@ -22,10 +24,13 @@ function App() {
       <Route path='/signup' element={<SignUp/>}/>
       <Route path='/forgot-password' element={<ForgotPassword/>}/>
       <Route path='/reset-password/:token' element={<ResetPassword/>}/>
-      <Route path='/complaint' element={<Complaint/>}/>
+      <Route path='/complaint/:id' element={<Complaint/>}/>
+      <Route path='/track-complaint' element={<TrackComplaint/>}/>
+      <Route path='/edit-complaint' element={<EditComplaint/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
 
       
+      <Route path='' element={<Navigate to={'/landing-page'}/>}/>
       <Route path='*' element={<DefaultPage/>}/>
 
 
